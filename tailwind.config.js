c40e8872a2680cebf2013c8155a6a2a8c71e30a8
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2024-03-21 00:08:50 -0300

    Finalizado formulário e adicionado algumas animações

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "entrada-letras": "moverLetras 2s ease-out forwards",
+      },
+      keyframes: {
+        moverLetras: {
+          "0%": { transform: "translateZ(-1000px)", opacity: "0" },
+          "100%": { transform: "translateZ(0)", opacity: "1" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
